fce612c0a3e7fdcbea847301705507f6d85f1bbd
Nathan Caza <mastercactapus@gmail.com>
2014-11-01 14:42:20 -0500

    stuff to work with unit files

---
+++ emissary/unit-file.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	sdunit "github.com/coreos/go-systemd/unit"
+	"io/ioutil"
+	"path"
+	"strings"
+)
+
+// UnitFile represents a systemd unit
+type UnitFile struct {
+	Options []*sdunit.UnitOption
+	Name    string
+}
+
+// UnitTypeFromName returns the type from a systemd unit name
+func UnitTypeFromName(name string) string {
+	i := strings.LastIndex(name, ".")
+	if i == -1 {
+		return ""
+	}
+	return name[i+1:]
+}
+
+// UnitPrefixFromName returns the prefix of a systemd unit name
+func UnitPrefixFromName(name string) string {
+	i := strings.Index(name, "@")
+	if i == -1 {
+		i = strings.LastIndex(name, ".")
+		if i == -1 {
+			return ""
+		}
+	}
+	return name[:i]
+}
+
+// UnitInstanceFromName returns the instance of a systemd unit
+func UnitInstanceFromName(name string) string {
+	s := strings.Index(name, "@")
+	if s == -1 {
+		return ""
+	}
+	e := strings.LastIndex(name, ".")
+	if e == -1 {
+		return ""
+	}
+	return name[s+1 : e]
+}
+
+func (u *UnitFile) Prefix() string {
+	return UnitPrefixFromName(u.Name)
+}
+func (u *UnitFile) Type() string {
+	return UnitTypeFromName(u.Name)
+}
+func (u *UnitFile) Instance() string {
+	return UnitInstanceFromName(u.Name)
+}
+func (u *UnitFile) Serialize() []byte {
+	read := sdunit.Serialize(u.Options)
+	data, _ := ioutil.ReadAll(read)
+	return data
+}
+
+// NewUnitFile creates a new parsed UnitFile with the given name and data
+func NewUnitFile(name string, data []byte) (unit *UnitFile, err error) {
+	read := bytes.NewReader(data)
+	opts, err := sdunit.Deserialize(read)
+	if err != nil {
+		return
+	}
+	return &UnitFile{Name: name, Options: opts}, nil
+}
+
+// NewUnitFromFile creates a new parsed UnitFile, using the basename as the unit name
+func NewUnitFromFile(unitFilePath string) (unit *UnitFile, err error) {
+	data, err := ioutil.ReadFile(unitFilePath)
+	if err != nil {
+		return
+	}
+	return NewUnitFile(path.Base(unitFilePath), data)
+}
---
+++ emissary/unit-store.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/armon/consul-api"
+)
+
+type UnitStore struct {
+	kv *consulapi.KV
+}
+
+const PrefixUnitFiles = "emissary/unit-files/"
+
+var ErrInvalidUnitName = errors.New("Invalid unit name")
+var ErrUnitNotFound = errors.New("No unit exists in store by that name")
+
+func NewUnitStore(c *consulapi.Client) *UnitStore {
+	return &UnitStore{kv: c.KV()}
+}
+
+func (s *UnitStore) Find(unitName string) (unit *UnitFile, unitVersion string, err error) {
+
+	unit, unitVersion, err = s.GetLatest(unitName)
+	if err != nil && strings.Contains(unitName, "@") {
+		unitName = UnitPrefixFromName(unitName) + "@" + UnitTypeFromName(unitName)
+		unit, unitVersion, err = s.GetLatest(unitName)
+	}
+
+	return
+}
+
+func (s *UnitStore) GetLatest(unitName string) (unit *UnitFile, unitVersion string, err error) {
+	unitVersion, err = s.GetLatestVersion(unitName)
+	if err != nil {
+		return
+	}
+	unit, err = s.Get(unitName, unitVersion)
+	return
+}
+func (s *UnitStore) GetLatestVersion(unitName string) (unitVersion string, err error) {
+	val, _, err := s.kv.Get(PrefixUnitFiles+unitName+"/latest", nil)
+	if err != nil {
+		return
+	}
+
+	return string(val.Value), nil
+}
+func (s *UnitStore) Get(unitName, tag string) (unit *UnitFile, err error) {
+	val, _, err := s.kv.Get(PrefixUnitFiles+unitName+"/"+tag, nil)
+	if err != nil {
+		return
+	}
+
+	unit, err = NewUnitFile(unitName, val.Value)
+	return
+}
+func (s *UnitStore) Set(unitName string, unit *UnitFile) error {
+	data := unit.Serialize()
+	sum := sha256.Sum224(data)
+	hash := hex.EncodeToString(sum[:])
+	_, err := s.kv.Put(&consulapi.KVPair{Key: PrefixUnitFiles + unitName + "/" + hash, Value: data}, nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.kv.Put(&consulapi.KVPair{Key: PrefixUnitFiles + unitName + "/latest", Value: []byte(hash)}, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (s *UnitStore) Exists(unitName string) (bool, error) {
+	_, _, err := s.kv.Get(PrefixUnitFiles+unitName+"/latest", nil)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (s *UnitStore) List(filter string) ([]string, error) {
+	list, _, err := consul.KV().Keys("emissary/unit-files/", "/", nil)
+
+	if err != nil {
+		fmt.Println("Failed to list units:", err)
+		os.Exit(2)
+	}
+	names := make([]string, 0, len(list))
+	for _, v := range list {
+		name := path.Base(v[:len(v)-1])
+		if m, _ := filepath.Match(filter, name); m {
+			names = append(names, name)
+		}
+	}
+
+	return names, nil
+}
+
+func (s *UnitStore) Delete(unitName string) error {
+	_, err := s.kv.Delete(PrefixUnitFiles+unitName+"/latest", nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UnitStore) DeleteAll(unitName string) error {
+	_, err := s.kv.DeleteTree(PrefixUnitFiles+unitName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
